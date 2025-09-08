d2ce29b56c4d0360e3f19597f41562aab667e179
Antony Njoroge <antonjoro2008@gmail.com>
2025-09-08 10:45:07 +0300

    changes

--- resources/views/assessments.blade.php
+++ resources/views/assessments.blade.php
@@ -20,7 +20,7 @@
         </div>
     </div>
     
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <!-- Loading State -->
         <div id="assessmentsLoading" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <!-- Loading Skeleton Cards -->
